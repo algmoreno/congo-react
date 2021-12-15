fd778cd3647834ec826e95d49961cd6d46edd4f2
algmoreno <alg.moreno00@gmail.com>
2021-12-15 11:27:30 -0700

    package jsons

--- server/config/connection.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/congo-react', 
-{
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-});
-
-module.exports = mongoose.connection;
