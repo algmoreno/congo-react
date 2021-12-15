9ea6003e479389fd4a6e10a90c510490847004aa
algmoreno <alg.moreno00@gmail.com>
2021-12-15 12:48:53 -0700

    typeDefs

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,40 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+  type User {
+    _id: ID
+    firstName: String
+    lastName: String
+    username: String
+    email: String
+    posts: [Post]
+  }
+
+  type Post {
+    _id: ID
+    postText: String
+    createdAt: String
+    username: String
+  }
+
+  type Auth {
+    token: ID!
+    user: USer
+  }
+
+  type Query {
+    me: User
+    users: [User]
+    user(username: String!): User
+    posts(username: String): [Post]
+    post(_id: ID!): Post
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(firstName: String!, lastName: String!, username: String!, email: String!, password: String!): Auth
+    addPost(postText: String!): Post
+  }
+`;
+
+module.exports = typeDefs; 
